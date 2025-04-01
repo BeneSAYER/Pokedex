7ab79b8ef0a88dfd864a756bf0375c5a6fab5a61
Bene SAYER <bentouster@gmail.com>
2025-04-01 15:56:30 +0200

    rendu dynamique de la PokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,15 +1,27 @@
-function App() {
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+]
+
+function PokemonCard() {
+  const pokemon = pokemonList[1]
   return (
-    <figure>
-      <img>
-        src : https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png
-      </img>
-      <figcaption>
-        <Bulbasaur />
-      </figcaption>
-    </figure>;
+    <article>
+      <h1>{pokemon.name}</h1>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+    </article>
   );
 }
-export default App;
+// ...
+
+
+export default PokemonCard;
 
 
