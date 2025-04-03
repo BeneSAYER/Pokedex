a47f45f97bea3a2fa50331dcd484140a257ee30d
Bene SAYER <bentouster@gmail.com>
2025-04-03 16:54:11 +0200

    répéter un bloc d'affichage avec map

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,21 @@ const pokemonList = [
         imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+        name: "charmander",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+        name: "squirtle",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+        name: "pikachu",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
         name: "mew",
         imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
     },
@@ -28,9 +43,18 @@ function App() {
 
     return (
         <div>
+            <nav>
+                {pokemonList.map((pokemon, index) => (
+
+                    <button key={index} type="button" onClick={() => setPokemonName(pokemon.name)}>
+                        {pokemon.name}
+                    </button>
+                ))}
+
+            </nav>
             <PokemonCard pokemon={pokemon} />
 
-        </div>
+        </div >
     );
 }
 
