be6b76387957ff2ccf8abf3fd8adc15bbc1b8b7e
Bene SAYER <bentouster@gmail.com>
2025-04-03 15:42:18 +0200

    ajout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import "./App.css";
 
+import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 const pokemonList = [
     {
         name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
         name: "mew",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
     },
 ];
 
-
 function App() {
+
+    const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+
+    const pokemon = pokemonList.find((p) => p.name === pokemonName);
+
+
+    if (!pokemon) {
+        throw new Error("Invalid Pokémon name");
+    }
+
     return (
         <div>
-            <PokemonCard pokemon={pokemonList[1]} />
+            <PokemonCard pokemon={pokemon} />
+            <button type="button" onClick={() => setPokemonName("bulbasaur")}>Bulbasaur</button>
+            <button type="button" onClick={() => setPokemonName("mew")}>Mew</button>
         </div>
-
     );
 }
 
